b73245d2f45b34a2bc74e88f8900a69b67f8d100
Natalia <gon4arovanatalija@gmail.com>
2018-12-22 23:03:29 +0200

    otions changed on dropdown click

--- src/priority-react-components/components/Inputs/CustomerName/customerName.tsx
+++ src/priority-react-components/components/Inputs/CustomerName/customerName.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import classNames from "classnames";
 // import { ComponentBaseProperties, ComponentBaseState, ComponentBase, ComponentBaseSkin } from "../../../base-classes";
-import Icon from '../../Icon';
+import Icon from "../../Icon";
 import "./customerName.scss";
 
 // type customerNameSkin = ComponentBaseSkin | "Stroke" | "RoundStrokeIcon";
@@ -16,36 +16,37 @@ import "./customerName.scss";
 // }
 
 export default class customerName extends React.Component {
-
-  state={
-    isOpen:false
-  }
-  DropDownToggle=()=>{
-    this.setState({isOpen:!this.state.isOpen});
-  }
+  state = {
+    isOpen: false
+  };
+  DropDownToggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
   public render() {
-// console.log(this.state.isOpen);
-let classForDropDownIcon;
-if(this.state.isOpen){
-  classForDropDownIcon="icon-icon-arrow_drop_up";
-}else{
-  classForDropDownIcon="icon-icon-arrow_drop_down";
-}
+    let classForDropDownIcon;
+    if (this.state.isOpen) {
+      classForDropDownIcon = "icon-icon-arrow_drop_up";
+    } else {
+      classForDropDownIcon = "icon-icon-arrow_drop_down";
+    }
+    const dropDownOptionsClass = "options-wrapper " + (this.state.isOpen ? "" : "drop-down-hide");
+
     return (
       <div className="container-for-drop-down">
         <span className="drop-down-title">Customer name</span>
         <div className="drop-down-inputs">
-          <div className="input-small" onClick={this.DropDownToggle} >
-          <div className="drop-down_icon-wrapper">
-          <Icon fontIconClass={classForDropDownIcon}/></div>
+          <div className="input-small" onClick={this.DropDownToggle}>
+            <div className="drop-down_icon-wrapper">
+              <Icon fontIconClass={classForDropDownIcon} />
+            </div>
           </div>
-          <div className="options-wrapper">
+          <div className={dropDownOptionsClass}>
             <span className="options-to-choose">Volvo</span>
             <span className="options-to-choose">Saab</span>
             <span className="options-to-choose">Mercedes</span>
             <span className="options-to-choose">Audi</span>
           </div>
-          <select className="input-big"/>
+          <select className="input-big" />
         </div>
         <div className="drop-down-resalts-container">
           <span className="drop-down-resalt">Stormie Hansford</span>
