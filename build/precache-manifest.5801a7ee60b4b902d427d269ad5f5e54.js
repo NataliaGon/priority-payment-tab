96a52aa3d18061ae00600b823cd757fce22a93d4
Anton Dorofeyev <dorofeyev@gmail.com>
2018-12-23 16:08:08 +0200

    rebuild

---
+++ build/precache-manifest.5801a7ee60b4b902d427d269ad5f5e54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "56fa696de4a885e3f8c2",
+    "url": "/static/js/main.56fa696d.chunk.js"
+  },
+  {
+    "revision": "54658df75ed744602958",
+    "url": "/static/js/1.54658df7.chunk.js"
+  },
+  {
+    "revision": "56fa696de4a885e3f8c2",
+    "url": "/static/css/main.1be3c844.chunk.css"
+  },
+  {
+    "revision": "ab6b3c0049cc7011d77c8445a177a1aa",
+    "url": "/index.html"
+  }
+];
