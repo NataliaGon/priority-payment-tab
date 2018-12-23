784e82c4d8b89251608ee39c48ce63a31e622ef3
Anton Dorofeyev <dorofeyev@gmail.com>
2018-12-23 13:23:14 +0200

    wip

--- src/priority-react-components/components/Inputs/TextInput/TextInput.tsx
+++ src/priority-react-components/components/Inputs/TextInput/TextInput.tsx
@@ -8,16 +8,67 @@ class TextInputProperties extends ComponentBaseProperties {
 }
 
 interface TextInputState extends ComponentBaseState {
-
+  isOpen?: boolean;
 }
 
 export default class TextInput extends ComponentBase<TextInputProperties, TextInputState> {
+
+  dropDownToggle = () => {
+    this.setState({ isOpen: !(this.state && this.state.isOpen) });
+  };
+
   public render() {
 
-    const elementClasses = classNames(styles.component, this.props.componentClasses);
+    const dropDownVisibilityClass = (this.state && this.state.isOpen) ? styles.expanded : null;
+    const elementClasses = classNames(styles.component, this.props.componentClasses, dropDownVisibilityClass);
 
     return (
-      <input type="text" />
+      <div className={elementClasses}>
+        <div className={classNames(styles.option, styles.active, styles.placeholder)} data-value="placeholder">Choose wisely</div>
+        <div className={styles.option} data-value="wow">
+          Wow!</div>
+        <div className={styles.option} data-value="drop">
+          So dropdown!</div>
+        <div className={styles.option} data-value="select">
+          Very select!</div>
+        <div className={styles.option} data-value="custom">
+          Much custom!</div>
+        <div className={styles.option} data-value="animation">
+          Such animation!</div>
+      </div>
     );
   }
 }
+
+// $(document).ready(function () {
+//   $(".drop .option").click(function () {
+//     var val = $(this).attr("data-value"),
+//       $drop = $(".drop"),
+//       prevActive = $(".drop .option.active").attr("data-value"),
+//       options = $(".drop .option").length;
+//     $drop.find(".option.active").addClass("mini-hack");
+//     $drop.toggleClass("visible");
+//     $drop.removeClass("withBG");
+//     $(this).css("top");
+//     $drop.toggleClass("opacity");
+//     $(".mini-hack").removeClass("mini-hack");
+//     if ($drop.hasClass("visible")) {
+//       setTimeout(function () {
+//         $drop.addClass("withBG");
+//       }, 400 + options * 100);
+//     }
+//     triggerAnimation();
+//     if (val !== "placeholder" || prevActive === "placeholder") {
+//       $(".drop .option").removeClass("active");
+//       $(this).addClass("active");
+//     };
+//   });
+
+//   function triggerAnimation() {
+//     var finalWidth = $(".drop").hasClass("visible") ? 22 : 20;
+//     $(".drop").css("width", "24em");
+//     setTimeout(function () {
+//       $(".drop").css("width", finalWidth + "em");
+//     }, 400);
+//   }
+// });
