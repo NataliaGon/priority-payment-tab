1c8aef6be3fdf728c6e671163a04e083036e433f
naomi <naomipol@audyx.com>
2019-01-06 09:32:58 +0200

    fix button background

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import App from './App'
+import App from './Buttons'
 
 ReactDOM.render(<App />, document.getElementById('root'))
