b4703900aa580a2efdd2d1a29e9471177eec45b9
Natalia Goncharova <natalia.g@morning.agency>
2019-01-07 16:54:24 +0200

    labes ClassNames api

--- src/components/Text/Label/Label.tsx
+++ src/components/Text/Label/Label.tsx
@@ -6,11 +6,10 @@ import { ComponentBaseProperties, ComponentBaseState, ComponentBase } from "../.
 import styles from "./Label.module.scss";
 
 type LabelSize = "large" | "regular" | "regular-high-contrast" | "small" | "bold" | "small-bold";
-type LabelPadding = "regular" | "double";
+
 
 interface LabelProperties extends ComponentBaseProperties {
   size?: LabelSize;
-  labelPadding?: LabelPadding;
   textHref?: string;
 }
 
@@ -21,31 +20,11 @@ interface LabelState extends ComponentBaseState {
 export class Label extends ComponentBase<LabelProperties, LabelState> {
   public render() {
 
-    let sizeClass = "";
-    if (this.props.size) {
-      sizeClass = styles[this.props.size];
-    }
 
-    let paddingClass = "";
-    if (this.props.labelPadding) {
-      paddingClass = styles["padding-" + this.props.labelPadding];
-    }
-
-    let linkClass = "";
-    if (this.props.textHref) {
-      linkClass = styles.href;
-    }
-
-    let colorClass = "grey";
-
-    if (this.props.textColor) {
-
-        colorClass = styles[this.props.textColor];
+    let classNames = require('classnames/bind');
+    let cx = classNames.bind(styles);
+    const elementClasses = cx('component',this.props.size, this.props.textColor, {'href':this.props.textHref});
     
-    }
-
-    const elementClasses = classNames(styles.component, this.props.componentClasses, sizeClass, paddingClass, linkClass, colorClass);
-
     return (
       this.props.textHref ?
         <a className={elementClasses} href={this.props.textHref}>{this.props.text ? this.props.text : this.props.children}</a>
