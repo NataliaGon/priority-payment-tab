2fe8c628e9ce26b8ab7d087dc0f2227b162537c7
Natalia Goncharova <natalia.g@morning.agency>
2019-06-03 21:33:26 +0300

    +

--- example/src/example-views/tabs-example/TabsExample.tsx
+++ example/src/example-views/tabs-example/TabsExample.tsx
@@ -1,6 +1,5 @@
 import React, { ComponentElement } from 'react';
 import styles from './TabsExample.module.scss';
-// import 'rc-tabs/assets/index.css';
 import { Tabs, TabPane } from 'priority-style-react';
 
 
@@ -32,7 +31,7 @@ const TabsExample: () => ComponentElement<PanelContentProperties, any> = () => {
                 </Tabs>
             </div>
 
-            <div dir="rtl">
+            <div id="dir" dir="rtl">
                 <h1>Tabs Example RTL</h1>
                 <div className={styles.tabsContainer}>
                     <Tabs
@@ -47,20 +46,6 @@ const TabsExample: () => ComponentElement<PanelContentProperties, any> = () => {
                     </Tabs>
                 </div>
             </div>
-        
-            {/* <h1>Tabs Example</h1>
-            <div className={styles.tabsContainer}>
-                <Tabs
-                    defaultActiveKey="0"
-                    tabBarPosition="right">
-                    <TabPane tab={'My open invoces (3)'} key="0" >
-                        <PanelContent id='1' />
-                    </TabPane>
-                    <TabPane tab={'Payments Made (3)'} key="1" >
-                        <PanelContent id='2' />
-                    </TabPane>
-                </Tabs>
-            </div> */}
 
         </div>
     );
@@ -69,84 +54,5 @@ const TabsExample: () => ComponentElement<PanelContentProperties, any> = () => {
 export { TabsExample };
 export default TabsExample;
 
-
-// import 'rc-tabs/assets/index.css';
-// import React from 'react';
-// import classNames from 'classnames/bind';
-// import ReactDOM from 'react-dom';
-// import {Tabs,  TabPane } from 'priority-style-react';
-// import TabContent from 'rc-tabs/lib/SwipeableTabContent';
-// import ScrollableInkTabBar from 'rc-tabs/lib/ScrollableInkTabBar';
-
-
-// const PanelContent = ({ id }) => (
-//     <div style={{ height: 200, overflow: 'auto' }}>Tabular Data {id}</div>
-// );
-
-// export default class TabsExample extends React.Component {
-//     state = {
-//         activeKey: '1',
-//         start: 0,
-//     };
-
-//     onChange = (activeKey) => {
-//         console.log(`onChange ${activeKey}`);
-//         this.setState({
-//             activeKey,
-//         });
-//     }
-
-//     onTabClick = (key) => {
-//         console.log(`onTabClick ${key}`);
-//         if (key === this.state.activeKey) {
-//             this.setState({
-//                 activeKey: '',
-//             });
-//         }
-//     }
-
-
-//     render() {
-//         const start = this.state.start;
-//         return (
-//             <div >
-//                 <h1>Tabs Example</h1>
-//                 <Tabs
-//                     // renderTabBar={() => <ScrollableInkTabBar onTabClick={this.onTabClick} />}
-//                     // renderTabContent={() => <TabContent animatedWithMargin />}
-//                     // activeKey={this.state.activeKey}
-//                     // onChange={this.onChange}
-//                     // defaultActiveKey="1"
-//                 >
-//                     <TabPane tab={`My open invoces (3)`} key="1">
-//                         <PanelContent id={start} />
-//                     </TabPane>
-//                     <TabPane tab={`Payments Made (3)`} key="2">
-//                         <PanelContent id={start + 1} />
-//                     </TabPane>
-//                 </Tabs>
-//                 <div dir="rtl">
-//                 <h1>Tabs Example RTL</h1>
-//                 <Tabs
-//                     // renderTabBar={() => <ScrollableInkTabBar onTabClick={this.onTabClick} />}
-//                     // renderTabContent={() => <TabContent animatedWithMargin />}
-//                     // activeKey={this.state.activeKey}
-//                     // onChange={this.onChange}
-//                     // defaultActiveKey="1"
-//                 >
-//                     <TabPane tab={`הפתיחות הפתוחות שלי (3)`} key="1">
-//                         <PanelContent id={start} />
-//                     </TabPane>
-//                     <TabPane tab={`תשלומים שבוצעו (3)`} key="2">
-//                         <PanelContent id={start + 1} />
-//                     </TabPane>
-//                 </Tabs>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
-
-
-
-
+// const dir = document.getElementById('dir');
+// console.log(dir);
