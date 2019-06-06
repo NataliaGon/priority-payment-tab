f997bbf3b9fb7cf8b66b4a3ba29a339143ed21f8
morning-natalia <natalia.g@morning.agency>
2019-06-06 11:12:06 +0300

    table

---
+++ src/components/Tabel/Tabel.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import classNames from 'classnames/bind';
+import { ComponentBaseProperties } from '../../core';
+import styles from './Tabel.module.scss';
+
+const cx = classNames.bind(styles);
+
+
+interface Tabel extends  ComponentBaseProperties {
+
+}
+
+export  class Tabel extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
